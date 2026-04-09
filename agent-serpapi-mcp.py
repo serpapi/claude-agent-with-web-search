220e04c7ba6f82fb8bec287e6e0e5775fdae3783
hilmanski <hilmanhgb@gmail.com>
2026-04-09 10:34:38 +0800

    extract system prompt on mcp sample

--- agent-serpapi-mcp.py
+++ agent-serpapi-mcp.py
@@ -17,7 +17,14 @@ async def main():
     start_time = time.perf_counter() #log time
     print("Running...")
 
+    # USER_PROMPT = "Use the MCP server to explain what it does and how to use it."
+    # USER_PROMPT = "Use the MCP server; What's trending news for AI? share links if possible."
+
+    SYSTEM_PROMPT = "Always use the SerpApi MCP server for web search;" # Pro tips add system prompt
+    USER_PROMPT = "Find me a hotel under $100 in Bay Area next week."
+
     options = ClaudeAgentOptions(
+        system_prompt=SYSTEM_PROMPT,
         mcp_servers={
             "serpapi": {
                 "type": "http",
@@ -26,12 +33,6 @@ async def main():
         },
         allowed_tools=["mcp__serpapi__*"], #Base name:mcp__<server-name>__<tool-name>
     )
-
-    # USER_PROMPT = "Use the MCP server to explain what it does and how to use it."
-    # USER_PROMPT = "Use the MCP server; What's trending news for AI? share links if possible."
-
-    SYSTEM_PROMPT = "Always use the SerpApi MCP server for web search;" # Pro tips add system prompt
-    USER_PROMPT = SYSTEM_PROMPT + "Find me a hotel under $100 in Bay Area next week."
 
     async for message in query(
         prompt=USER_PROMPT,
